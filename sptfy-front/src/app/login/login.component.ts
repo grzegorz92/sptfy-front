92f626af3b6d8d3839dcc099504568ea610e85df
grjk <grjk@ailleron.com>
2018-07-25 16:38:26 +0200

    modify login component

--- sptfy-front/src/app/login/login.component.ts
+++ sptfy-front/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  background: string;
+  backgroundUrls = [];
 
   ngOnInit() {
+    this.getBackground();
+  }
+
+
+  getBackground() {
+
+    this.backgroundUrls.push("https://img.wallpapersafari.com/desktop/1920/1080/16/37/7gBJDt.jpg");
+    this.backgroundUrls.push("http://hapiclomap.com/wp-content/uploads/2017/02/Hipster-Wallpaper-343-Perfect-Image-Hd-Backgrounds.jpg");
+    this.backgroundUrls.push("http://cheaprbsunglassoutelt.us/wp-content/uploads/2018/03/hipster-wallpaper-fresh-hipster-wallpaper-of-hipster-wallpaper.jpg");
+    this.backgroundUrls.push("https://livewallpaper.info/wp-content/uploads/2017/08/full-hd-hipster-wallpaper-wpc9005349-1024x640.jpg");
+    this.background = this.backgroundUrls[Math.floor(Math.random() * this.backgroundUrls.length)];
+    // this.background = "https://livewallpaper.info/wp-content/uploads/2017/08/full-hd-hipster-wallpaper-wpc9005349-1024x640.jpg";
   }
 
 }
