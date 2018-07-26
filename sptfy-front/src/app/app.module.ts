5ee9b5b0a019760c436cd01f15d02c213e090d39
grjk <grjk@ailleron.com>
2018-07-26 16:07:27 +0200

    develop app

--- sptfy-front/src/app/app.module.ts
+++ sptfy-front/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterComponent } from './register/register.component';
+import { IndexMenuComponent } from './index-menu/index-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    IndexMenuComponent
   ],
   imports: [
     BrowserModule
