98a62071c135dc7d130657dda6b16be7169059ec
grjk <grjk@ailleron.com>
2018-07-27 14:39:21 +0200

    added new components

---
+++ sptfy-front/src/app/index-menu/index-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-menu',
+  templateUrl: './index-menu.component.html',
+  styleUrls: ['./index-menu.component.css']
+})
+export class IndexMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
